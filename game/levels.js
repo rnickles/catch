2e631a45ebfdf139da1584f04556834fda0b21da
rnickles <robbynickles@gmail.com>
2024-05-21 23:11:23 -0700

    minor tweaks

---
+++ game/levels.js
@@ -0,0 +1,42 @@
+import { Goal, Platform } from "./objects/platform.js"
+
+export function init_level1(engine, game_state) {
+    let goal = new Goal(200, 800, engine, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+}
+
+export function init_level2(engine, game_state) {
+    let goal = new Goal(800, 800, engine, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+}
+
+export function init_level3(engine, game_state) {
+    let goal = new Goal(200, 800, engine, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+    new Platform(89, 630, 350, 630, engine);
+}
+
+export function init_level4(engine, game_state) {
+    let goal = new Goal(800, 800, engine, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+    new Platform(650, 229, 668, 833, engine);
+
+}
+
+export function init_level5(engine, game_state) {
+    let goal = new Goal(200, 800, engine, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+    new Platform(83, 730, 431, 783, engine);
+    new Platform(331, 732, 321, 849, engine)
+}
+
+// Return a list of level intialializers; 1st item is null since there is no level 0
+// They all accept the same exact argumets: engine and game_state.
+export function getLevelInitializers() {
+    return [null, init_level1, init_level2, init_level3, init_level4, init_level5];
+}
--- game/levels/level5.js
+++
@@ -1,9 +0,0 @@
-import { Goal, Platform } from "../objects/platform.js"
-
-export function init_level5(engine, game_state) {
-    let goal = new Goal(200, 800, engine, game_state);
-    game_state.drop_coords = [450, 50];
-    game_state.goal = goal;
-    new Platform(83, 730, 431, 783, engine);
-    new Platform(331, 732, 321, 849, engine)
-}
