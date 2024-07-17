9ff49cb116966952df3680325d6ea1feaa9af70f
rnickles <robbynickles@gmail.com>
2024-07-17 16:16:50 -0700

    moved objects up, for some reason things weren't visible

--- game/levels.js
+++ game/levels.js
@@ -1,41 +1,41 @@
 import { Goal, GoodPlatform, BouncyPlatform, MovingPlatform } from "./objects/platform.js"
 
 export function initLevel1(engine, gameState) {
-    new Goal(200, 800, engine, gameState);
+    new Goal(200, 600, engine, gameState);
     gameState.dropCoords = [450, 50];
     let mp = new MovingPlatform(400,400,450,400, engine);
     gameState.gameObjectsToUpdate.push(mp);
 }
 
 export function initLevel2(engine, gameState) {
-    new Goal(800, 800, engine, gameState);
+    new Goal(800, 600, engine, gameState);
     gameState.dropCoords = [450, 50];
 }
 
 export function initLevel3(engine, gameState) {
-    new Goal(200, 800, engine, gameState);
+    new Goal(200, 600, engine, gameState);
     gameState.dropCoords = [450, 50];
-    new GoodPlatform(89, 630, 350, 630, engine);
+    new GoodPlatform(89, 430, 350, 430, engine);
 }
 
 export function initLevel4(engine, gameState) {
-    new Goal(800, 800, engine, gameState);
+    new Goal(800, 600, engine, gameState);
     gameState.dropCoords = [450, 50];
     new GoodPlatform(650, 229, 668, 833, engine);
 
 }
 
 export function initLevel5(engine, gameState) {
-    new Goal(250, 800, engine, gameState);
+    new Goal(250, 600, engine, gameState);
     gameState.dropCoords = [450, 50];
-    new GoodPlatform(150, 700, 400, 700, engine);
-    new GoodPlatform(400, 700, 400, 849, engine)
+    new GoodPlatform(150, 500, 400, 500, engine);
+    new GoodPlatform(400, 500, 400, 649, engine)
 }
 
 export function initLevel6(engine, gameState) {
     new Goal(800, 600, engine, gameState);
     gameState.dropCoords = [250, 500]; 
-    new BouncyPlatform(300, 800, 500, 800, engine);
+    new BouncyPlatform(300, 600, 500, 600, engine);
 }
 
 // Return a list of level intialializers; 1st item is null since there is no level 0
