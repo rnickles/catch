8331ade805547352bf83a0419d58176a9fbbb5d9
rnickles <robbynickles@gmail.com>
2024-08-12 13:20:20 -0700

    turn off sound for deployment

--- game/objects/ball.js
+++ game/objects/ball.js
@@ -3,7 +3,7 @@ import { GameObject, CATEGORY_MOUSE, CATEGORY_DEFAULT } from "./gameObject.js";
 const DOT_SIZE = 50;
 
 export class Ball extends GameObject {
-    
+
     constructor(x, y, engine) {
         super();
         // matter stuff
@@ -25,7 +25,25 @@ export class Ball extends GameObject {
         this.bod = bod;
     }
     collisionStart(bodyThatCollided) {
-        const ballCollideSound = document.getElementById("ballCollisionSound");
-        ballCollideSound.play();
+        if (bodyThatCollided.gameObject instanceof Ball) {
+            return;
+        } else {
+            const ballCollisionSound = new Audio('ballCollisionSound.mp3');
+            // Calculate the velocity magnitude (speed) of the ball
+            const velocity = Math.sqrt(
+                this.bod.velocity.x ** 2 +
+                this.bod.velocity.y ** 2
+            );
+
+            // Scale the volume based on the velocity
+            // Assume a maximum volume of 1, you can adjust the scaling factor as needed
+            const maxSpeed = 20; // Adjust this based on the expected maximum speed
+            const volume = Math.min(1, velocity / maxSpeed); // Ensure volume is between 0 and 1
+
+            // Set the volume of the sound
+            ballCollisionSound.volume = volume;
+            // ballCollisionSound.play();
+            ballCollisionSound.remove();
+        }
     }
 }
